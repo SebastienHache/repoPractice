ba785f8eb5bfb05f4a504b8128627b6fe835af8b
Sebastien Hache <sebastien-123@hotmail.com>
2025-08-21 21:06:06 -0300

    pushed on secondary branch

--- BranchPractice/app.js
+++ BranchPractice/app.js
@@ -1,1 +1,16 @@
-console.log('Hello there can you read this?');
+console.log("Hello there can you read this?");
+
+const btnCalculate = document.getElementById("btnCalculate");
+
+btnCalculate.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let miles = parseFloat(document.getElementById("txtMile").value);
+  let gallons = parseFloat(document.getElementById("txtGallon").value);
+  let divResult = document.getElementById('divResult');
+  let mpg = miles/ gallons;
+  console.log(miles, gallons, mpg );
+
+
+  divResult.innerHTML = `<p>You travelled ${miles} miles and consumed ${gallons} gallons. giving you a MPG of ${mpg}</p>`;
+});
